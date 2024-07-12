160512217e76094d40fd1b6a5cd0d835f1b55e72
zhuangqx1 <zhuangqx1@meicloud.com>
2024-07-12 09:29:23 +0800

    init

---
+++ src/main/java/org/example/jmetertest/JmeterTestApplication.java
@@ -0,0 +1,13 @@
+package org.example.jmetertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmeterTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JmeterTestApplication.class, args);
+    }
+
+}
